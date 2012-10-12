086831362a17d980e746a6641ec7514fb63f17c8
Peter Macko <pmacko@eecs.harvard.edu>
2012-10-12 12:21:36 -0400

    Added DexUtils.getEdgeTypes().

--- blueprints-extensions-impl/src/main/java/com/tinkerpop/blueprints/extensions/impls/dex/DexUtils.java
+++ blueprints-extensions-impl/src/main/java/com/tinkerpop/blueprints/extensions/impls/dex/DexUtils.java
@@ -1,6 +1,7 @@
 package com.tinkerpop.blueprints.extensions.impls.dex;
 
 import com.sparsity.dex.gdb.EdgesDirection;
+import com.sparsity.dex.gdb.Graph;
 import com.tinkerpop.blueprints.Direction;
 
 
@@ -25,5 +26,33 @@ public class DexUtils {
 		case BOTH: return EdgesDirection.Any;
 		default  : throw new IllegalArgumentException("Invalid direction"); 
 		}
+	}
+	
+	
+	/**
+	 * Get edge types
+	 * 
+	 * @param graph the DEX {@link Graph}
+	 * @param label the edge label or null to get all edge types
+	 * @return an array of edge types
+	 */
+	public static int[] getEdgeTypes(Graph graph, String label) {
+		
+		int[] types = null;
+		
+		if (label == null) {
+			com.sparsity.dex.gdb.TypeList tlist = graph.findEdgeTypes();
+			com.sparsity.dex.gdb.TypeListIterator typeItr = tlist.iterator();
+			types = new int[tlist.count()];
+			int ti = 0; while (typeItr.hasNext()) types[ti++] = typeItr.nextType();
+			tlist.delete();
+			tlist = null;
+		}
+		else {
+			types = new int[1];
+			types[0] = graph.findType(label);
+		}
+		
+		return types;
 	}
 }
